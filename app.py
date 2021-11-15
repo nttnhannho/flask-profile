67803e018c9eb4586e163d66a2c1bdc9eeed22a4
nttnhannho <makelifeperfect@gmail.com>
2021-11-15 16:41:45 +0700

    Init - Add contact feature

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import gspread
 
 
@@ -6,14 +6,17 @@ gc = gspread.service_account(filename="flask-profile.json")
 sh = gc.open("flask-profile")
 sh_profile = sh.get_worksheet(0)
 sh_contacts = sh.get_worksheet(1)
-sh_contacts.append_row(["Bob", "Bob@gmail.com", "Hi"])
 
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
+    if request.method == "POST":
+        sh_contacts.append_row([request.form["name"],
+                               request.form["email"],
+                               request.form["message"]])
     profile = {
         "about": sh_profile.acell("B1").value,
         "interests": sh_profile.acell("B2").value,
@@ -21,6 +24,11 @@ def home():
         "education": sh_profile.acell("B4").value
     }
     return render_template("index.html", profile=profile)
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    return render_template("contact.html")
 
 
 app.run(debug=True)
